37bcaadf702d03a6e2b7b3555262f0531fae40e4
davidlarcher <david.larcher@wanadoo.fr>
2019-06-22 11:13:28 +0200

    First commit

---
+++ ConvNet.py
@@ -0,0 +1,104 @@
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import scipy
+from PIL import Image
+from scipy import ndimage
+from utils import *
+from tensor_flow_utils import *
+
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+np.random.seed(1)
+
+np.random.seed(1)
+x = np.random.randn(4, 3, 3, 2)
+x_pad = zero_pad(x, 2)
+print ("x.shape =", x.shape)
+print ("x_pad.shape =", x_pad.shape)
+print ("x[1,1] =", x[1,1])
+print ("x_pad[1,1] =", x_pad[1,1])
+
+fig, axarr = plt.subplots(1, 2)
+axarr[0].set_title('x')
+axarr[0].imshow(x[0, :, :, 0])
+axarr[1].set_title('x_pad')
+axarr[1].imshow(x_pad[0, :, :, 0])
+plt.show()
+
+np.random.seed(1)
+a_slice_prev = np.random.randn(4, 4, 3)
+W = np.random.randn(4, 4, 3)
+b = np.random.randn(1, 1, 1)
+
+Z = conv_single_step(a_slice_prev, W, b)
+print("Z =", Z)
+
+np.random.seed(1)
+A_prev = np.random.randn(10,4,4,3)
+W = np.random.randn(2,2,3,8)
+b = np.random.randn(1,1,1,8)
+hparameters = {"pad" : 2,
+               "stride": 2}
+
+Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+print("Z's mean =", np.mean(Z))
+print("Z[3,2,1] =", Z[3,2,1])
+print("cache_conv[0][1][2][3] =", cache_conv[0][1][2][3])
+
+np.random.seed(1)
+A_prev = np.random.randn(2, 4, 4, 3)
+hparameters = {"stride" : 2, "f": 3}
+
+A, cache = pool_forward(A_prev, hparameters)
+print("mode = max")
+print("A =", A)
+print()
+A, cache = pool_forward(A_prev, hparameters, mode = "average")
+print("mode = average")
+print("A =", A)
+
+np.random.seed(1)
+dA, dW, db = conv_backward(Z, cache_conv)
+print("dA_mean =", np.mean(dA))
+print("dW_mean =", np.mean(dW))
+print("db_mean =", np.mean(db))
+
+# Loading the data (signs)
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Example of a picture
+index = 10
+plt.imshow(X_train_orig[index])
+plt.show()
+print ("y = " + str(np.squeeze(Y_train_orig[:, index])))
+
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+Y_train = convert_to_one_hot(Y_train_orig, 6).T
+Y_test = convert_to_one_hot(Y_test_orig, 6).T
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+conv_layers = {}
+
+
+tf.compat.v1.reset_default_graph()
+with tf.compat.v1.Session() as sess_test:
+    parameters = initialize_parameters()
+    init = tf.compat.v1.global_variables_initializer()
+    sess_test.run(init)
+    print("W1 = " + str(parameters["W1"].eval()[1,1,1]))
+    print("W2 = " + str(parameters["W2"].eval()[1,1,1]))
+
+_, _, parameters = model(X_train, Y_train, X_test, Y_test)
+
+
+
